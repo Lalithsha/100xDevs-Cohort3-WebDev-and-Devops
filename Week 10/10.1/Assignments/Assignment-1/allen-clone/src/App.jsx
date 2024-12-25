fdac9e9d3103ed082e7e15344786f447aa01d72f
Lalith Sharma <lalithsharma989@gmail.com>
2024-12-26 00:31:07 +0530

    Added routes in week 10.1's assignment code

--- Week 10/10.1/Assignments/Assignment-1/allen-clone/src/App.jsx
+++ Week 10/10.1/Assignments/Assignment-1/allen-clone/src/App.jsx
@@ -4,19 +4,60 @@ import './App.css'
 import Header from './components/Header'
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 
-
+function Courses(){
+  return (
+    <div>
+      <h1> neet course </h1>
+    </div>
+  )
+}
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+
+    {/* <Header/> */}
+    
     <BrowserRouter>
       <Routes>
-        <Route path='/' />
-            <Outlet/>
+        <Route index element={<Header/>}  />
+        <Route path='/neet' element={<Layout/>}>{/* This make sure that it's child route starts with neet */}
+        <Route path='/neet/courses' element={<Courses/>} />
+        <Route path="/neet/test-series" element={<TestSeries />} />
+        <Route path="/neet/Scholarships" element={<Scholarships />} />
+        </Route>
       </Routes>
     </BrowserRouter>
+    </>
+  )
+}
+
+/* {/* <Outlet/>  */
+
+function TestSeries(){
+  return (
+    <div>
+      <h1> Need online test series</h1>
+    </div>
+  )
+}
+function Scholarships(){
+  return (
+    <div>
+      <h1> adsat-register </h1>
+    </div>
+  )
+}
+
+function Layout(){
+  return (
+    <>
+    {/* <Header></Header>
+    <div>body</div>
+    <div>footer</div> */}
+    <Outlet/> {/* Outlet is used that in the browser router the element passed inside the route will be added here (children will be displayed here)  */}
     </>
   )
 }
