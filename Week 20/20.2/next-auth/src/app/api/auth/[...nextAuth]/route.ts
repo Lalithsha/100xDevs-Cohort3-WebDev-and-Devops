2e94d722815f151a30d974e8f01365e5ab9df53e
Lalith Sharma <lalithsharma989@gmail.com>
2025-03-11 00:19:03 +0530

    Added credentials provider, google provider using next-Auth

--- Week 20/20.2/next-auth/src/app/api/auth/[...nextAuth]/route.ts
+++ Week 20/20.2/next-auth/src/app/api/auth/[...nextAuth]/route.ts
@@ -1,7 +1,42 @@
 import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
 
 const handler = NextAuth({
-  provider:{}
+    providers: [
+        CredentialsProvider({
+          name: "Login with email",
+          credentials: {
+            username: { label: "Username", type: "text", placeholder: "ankuar@1" },
+            password: { label: "Password", type: "password" }
+          },
+          async authorize(credentials, req) {
+            const username = credentials?.username;
+            const password = credentials?.password;
+
+            // db request to check if the username and password are correct
+
+            const user = {
+                name: "lalith",
+                id: "1",
+                username:"lalith_1"
+            }
+            
+            if(user){
+                return user;
+            }
+            else {
+                return null;
+            }
+          }
+        }),
+        GoogleProvider({
+          clientId: "process.env.GOOGLE_CLIENT_ID",
+          clientSecret: "process.env.GOOGLE_CLIENT_SECRET"
+        })
+      ],
+      secret:process.env.NEXTAUTH_SECRET
 })
 
 export { handler as GET, handler as POST }
