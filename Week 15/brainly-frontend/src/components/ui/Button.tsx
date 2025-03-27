09079e2af4fd465b65dbaab8fd74964dd85ad2f0
Lalith Sharma <lalithsharma989@gmail.com>
2025-03-27 23:04:16 +0530

    Added a better approach for button component

--- Week 15/brainly-frontend/src/components/ui/Button.tsx
+++ Week 15/brainly-frontend/src/components/ui/Button.tsx
@@ -52,3 +52,54 @@ export const Button = ({
     </button>
   );
 };
+
+// Better approach.
+/* const getButtonClasses = (
+  variant: Variant,
+  size: Size,
+  fullWidth?: boolean,
+  loading?: boolean
+) => {
+  const base = "rounded-md shadow-md flex font-light items-center cursor-pointer";
+  const variants = {
+    primary: "bg-purple-600 text-white",
+    secondary: "bg-purple-200 text-purple-600",
+  };
+  const sizes = {
+    sm: "py-1 px-2",
+    md: "py-2 px-4",
+    lg: "py-4 px-6",
+  };
+
+  return [
+    base,
+    variants[variant],
+    sizes[size],
+    fullWidth ? "w-full justify-center" : "",
+    loading ? "opacity-45" : "",
+  ].join(" ");
+};
+
+export const Button = ({
+  variant,
+  size,
+  text,
+  startIcon,
+  endIcon,
+  onClick,
+  fullWidth,
+  loading,
+}: ButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={getButtonClasses(variant, size, fullWidth, loading)}
+      disabled={loading}
+    >
+      {startIcon && <div className="pr-2">{startIcon}</div>}
+      {text}
+      {endIcon}
+    </button>
+  );
+};
+ */
