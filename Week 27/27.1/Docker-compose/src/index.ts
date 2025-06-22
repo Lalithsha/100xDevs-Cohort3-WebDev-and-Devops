c7bc93b49e3666cdf4b20900f4348c2f6ad35b6c
Lalith Sharma <lalithsharma989@gmail.com>
2025-06-23 00:45:14 +0530

    Init a docker repo

---
+++ Week 27/27.1/Docker-compose/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { PrismaClient } from "./generated/prisma";
+const app = express();
+const prismaClient = new PrismaClient();
+
+app.get("/", async (req,res)=>{
+    const data = await prismaClient.user.findMany();
+    console.log("Get request received");
+    res.send({
+        "message":"Get end point",
+        data
+    })
+    
+})
+app.post("/", async (req,res)=>{
+
+    await prismaClient.user.create({
+        data:{
+            email:Math.random().toString(36).substring(2, 15) + "@example.com",
+            name:Math.random().toString(36).substring(2, 15),
+        }
+    })
+    res.send({
+        "message":"Get end point"
+    })
+    
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
