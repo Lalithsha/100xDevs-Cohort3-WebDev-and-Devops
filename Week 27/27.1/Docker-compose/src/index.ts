dbb5e666dc36ba63525772cf08e87cef6dad4cd6
Lalith Sharma <lalithsharma989@gmail.com>
2025-06-26 23:54:00 +0530

    Updated docker ignore, Dockerfile, docker-compose

--- Week 27/27.1/Docker-compose/src/index.ts
+++ Week 27/27.1/Docker-compose/src/index.ts
@@ -1,32 +1,32 @@
 import express from "express";
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from '@prisma/client';
 const app = express();
-const prismaClient = new PrismaClient();
+const prisma = new PrismaClient();
 
-app.get("/", async (req,res)=>{
-    const data = await prismaClient.user.findMany();
+app.get("/", async (req, res) => {
+    const data = await prisma.user.findMany();
     console.log("Get request received");
     res.send({
-        "message":"Get end point",
+        "message": "Get end point",
         data
     })
-    
-})
-app.post("/", async (req,res)=>{
 
-    await prismaClient.user.create({
-        data:{
-            email:Math.random().toString(36).substring(2, 15) + "@example.com",
-            name:Math.random().toString(36).substring(2, 15),
+})
+app.post("/", async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email: Math.random().toString(36).substring(2, 15) + "@example.com",
+            name: Math.random().toString(36).substring(2, 15),
         }
     })
     res.send({
-        "message":"Get end point"
+        "message": "Get end point"
     })
-    
+
 })
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
