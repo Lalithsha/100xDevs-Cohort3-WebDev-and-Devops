b308e195ecad961496d4db40c9c8dec01c7485b1
Lalith Sharma <lalithsharma989@gmail.com>
2025-07-26 21:13:47 +0530

    Added basic express app for ECS

---
+++ Week 30/30.1/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Hello world from Bun!");
+})
+
+
+app.get("/cpu", (req, res) => {
+	for (let i = 0; i < 100000000; i++) {
+		Math.random();
+	}
+	res.send("Hello world");
+});
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
+
